52d442dba867dc5a285197ba148f3a1c9514503e
vietnguyen0717 <vietnguyen0717@1fbad98d-3cfb-ac3b-e82d-718aab666f2b>
2011-03-04 15:35:33 +0000

    Update DAO_GramSetting.cs và Constants.cs

--- JCard/JCard/Constants.cs
+++ JCard/JCard/Constants.cs
@@ -9,5 +9,23 @@ namespace JCard
         public const int MAX_GRAMMAR_EXAMPLE = 10;
         public const int MAX_GRAMMAR_EXAMPLE_EXCEL = 10;
         public const string DATABASE_PATH = "datasource\\s3gt_db.mdb";
+        public const string Top = "0";
+        public const string Left = "0";
+        public const string BackColor = "-256";
+        public const string ForeColor = "-16777216";
+        public const string Width = "70";
+        public const string JP_BackColor = "-16724737";
+        public const string JP_ForeColor = "-16777216";
+        public const string JP_Width = "100";
+        public const string JP_IsDisplayed = "True";
+        public const string VN_BackColor = "-6697984";
+        public const string VN_ForeColor = "-16777216";
+        public const string VN_Width = "100";
+        public const string VN_IsDisplayed = "True";
+        public const string Ex_BackColor = "-4144960";
+        public const string Ex_ForeColor = "-16777216";
+        public const string Ex_NoOfDisplay = "6";
+        public const string Ex_DisplayTime = "5";
+        public const string Ex_DelayTime = "3";
     }
 }
