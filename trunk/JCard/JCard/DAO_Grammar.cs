1eeba1bdb1b591a500b2eca57e7738efbe871276
huynhngocminh@gmail.com <huynhngocminh@gmail.com@1fbad98d-3cfb-ac3b-e82d-718aab666f2b>
2011-03-04 09:02:25 +0000

    Merge FC-01

--- trunk/JCard/JCard/DAO_Grammar.cs
+++ trunk/JCard/JCard/DAO_Grammar.cs
@@ -58,6 +58,216 @@ namespace JCard
 
             return result;
         }
+
+        /// <summary>
+        /// Deletes all the grammar cards in S3GT database.
+        /// </summary>
+        /// <param name="strKyu">The String of kyu.</param>
+        /// <param name="dbFile">The S3GT databse file path.</param>
+        /// <returns></returns>
+        public Boolean DeleteGrammarCards(int strKyu, string dbFile)
+        {
+            OleDbConnection cn;
+            cn = DataProvider.ConnectData(dbFile);
+            //
+            string strSQL = "DELETE * FROM S3GT_GRAM WHERE Kyu=?";
+            try
+            {
+                OleDbCommand cmd = new OleDbCommand(strSQL, cn);
+                cmd.Parameters.Add("@Kyu", OleDbType.Integer);
+                cmd.Parameters["@Kyu"].Value = strKyu;
+                //
+                cmd.ExecuteNonQuery();
+                cmd.Cancel();
+                cn.Close();
+                return true;
+            }
+            catch (OleDbException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Inserts the grammar cards to S3GT_DB.
+        /// </summary>
+        /// <param name="arrDTOGram">The array of  DTO grammar cards.</param>
+        /// <param name="strKyu">The String of kyu.</param>
+        /// <param name="dbFile">The database file.</param>
+        /// <returns></returns>
+        public Boolean InsertGrammarCards(DTO_Grammar[] arrDTOGram, String strKyu, String dbFile)
+        {
+            OleDbConnection cn = null;
+            OleDbCommand cmd = null;
+            string strSQL = "";
+            int n = arrDTOGram.Length;
+            int i;
+            int j;
+            //           
+            try
+            {
+                //Initial String SQL
+                cn = DataProvider.ConnectData(dbFile);
+                string strSQL1 = "INSERT INTO S3GT_GRAM(Kyu,Sample,Syntax,Meaning_JP,Meaning_VN";
+                string strSQL2 = ") values (?,?,?,?,?";
+                //
+                j = 0;
+                for (; j < Constants.MAX_GRAMMAR_EXAMPLE; j++)
+                {
+                    strSQL1 += ",Example" + (j + 1).ToString();
+                    strSQL2 += ",?";
+                }
+                strSQL2 += ")";
+                strSQL = strSQL1 + strSQL2 + "";
+                //
+                i = 0;
+                for (; i < n; i++)
+                {
+                    cmd = new OleDbCommand(strSQL, cn);
+                    //Add Type of Value
+                    cmd.Parameters.Add("@Kyu", OleDbType.WChar);
+                    cmd.Parameters.Add("@Sample", OleDbType.WChar);
+                    cmd.Parameters.Add("@Syntax", OleDbType.WChar);
+                    cmd.Parameters.Add("@Meaning_JP", OleDbType.WChar);
+                    cmd.Parameters.Add("@Meaning_VN", OleDbType.WChar);
+
+                    //Add Values
+                    cmd.Parameters["@Kyu"].Value = strKyu;
+                    cmd.Parameters["@Sample"].Value = arrDTOGram[i].STR_Sample;
+                    cmd.Parameters["@Syntax"].Value = arrDTOGram[i].STR_Syntax;
+                    cmd.Parameters["@Meaning_JP"].Value = arrDTOGram[i].STR_Meaning_JP;
+                    cmd.Parameters["@Meaning_VN"].Value = arrDTOGram[i].STR_Meaning_VN;
+
+                    ////Examples
+                    j = 0;
+                    for (; j < Constants.MAX_GRAMMAR_EXAMPLE; j++)
+                    {
+                        String tmpStr = "@Example";
+                        tmpStr += (j + 1).ToString();
+                        cmd.Parameters.Add(tmpStr, OleDbType.WChar);
+                        cmd.Parameters[tmpStr].Value = arrDTOGram[i].ArrExample[j].ToString();
+                    }
+                    //Execute
+                    cmd.ExecuteNonQuery();
+                }
+                //Close
+                cmd.Cancel();
+                cn.Close();
+                return true;
+            }
+            catch (OleDbException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets the excel sheet names.
+        /// </summary>
+        /// <param name="excelFile">The excel file.</param>
+        /// <returns></returns>
+        static String[] GetExcelSheetNames(string excelFile)
+        {
+            OleDbConnection objConn = null;
+            System.Data.DataTable dt = null;
+            try
+            {
+                // Connection String. Change the excel file to the file you  
+                // will search.  
+                String connString = "Provider=Microsoft.Jet.OLEDB.4.0;" +
+                "Data Source=" + excelFile + ";Extended Properties=Excel 8.0;";
+                // Create connection object by using the preceding connection string. 
+                objConn = new OleDbConnection(connString);
+                objConn.Open();
+                // Lấy về dữ liệu   
+                dt = objConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                if (dt == null)
+                {
+                    return null;
+                }
+                String[] excelSheets = new String[dt.Rows.Count];
+                int i = 0;
+                // Add the sheet name to the string array.  
+                foreach (DataRow row in dt.Rows)
+                {
+                    excelSheets[i] = row["TABLE_NAME"].ToString();
+                    i++;
+                }
+                return excelSheets;
+            }
+            catch (OleDbException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+            finally
+            {
+                // Clean up.  
+                if (objConn != null)
+                {
+                    objConn.Close();
+                    objConn.Dispose();
+                }
+                if (dt != null)
+                {
+                    dt.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reads the excel file.
+        /// </summary>
+        /// <param name="exFilePath">The excel file path.</param>
+        /// <returns></returns>
+        public DTO_Grammar[] ReadExcelFile(String exFilePath)
+        {
+            DTO_Grammar[] result = null;
+            try
+            {
+                string sheetname = GetExcelSheetNames(exFilePath)[0];
+                string srcConnString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + exFilePath + @";Extended Properties=""Excel 8.0;HDR=YES;""";
+                string srcQuery = "SELECT * FROM[" + sheetname + "]";
+                OleDbConnection srcConn = new OleDbConnection(srcConnString);
+                srcConn.Open();
+                OleDbCommand objCmdSelect = new OleDbCommand(srcQuery, srcConn);
+                DataSet ds = new DataSet();
+                OleDbDataAdapter da = new OleDbDataAdapter(objCmdSelect);
+                da.Fill(ds, sheetname);
+                srcConn.Close();
+                //Check the right data
+                if (String.Compare(ds.Tables[0].Columns["MauCau"].ToString(), "MauCau") != 0)
+                    return result;
+                //Return to suitable result
+                int n = ds.Tables[0].Rows.Count;
+                result = new DTO_Grammar[n];
+                int i = 0;
+                for (; i < n; i++)
+                {
+                    result[i] = new DTO_Grammar();
+                    result[i].STR_Sample = ds.Tables[0].Rows[i]["MauCau"].ToString();
+                    result[i].STR_Syntax = ds.Tables[0].Rows[i]["CuPhap"].ToString();
+                    result[i].STR_Meaning_JP = ds.Tables[0].Rows[i]["YNghia_JP"].ToString();
+                    result[i].STR_Meaning_VN = ds.Tables[0].Rows[i]["YNghia_VN"].ToString();
+                    //Example
+                    int j = 0;
+                    for (; j < Constants.MAX_GRAMMAR_EXAMPLE; j++)
+                    {
+                        String tmpStr = "ViDu";
+                        tmpStr += (j + 1).ToString();
+                        result[i].ArrExample.Add(ds.Tables[0].Rows[i][tmpStr].ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return result;
+            }
+            return result;
+        }
         #endregion
     }
 }
--- trunk/JCard/JCard/fImportData.cs
+++ trunk/JCard/JCard/fImportData.cs
@@ -10,37 +10,123 @@ namespace JCard
 {
     public partial class fImportData : Form
     {
+        private Timer proTimer = new Timer();
+        /// <summary>
+        /// Initializes a new instance of the <see cref="fImportData"/> class.
+        /// </summary>
         public fImportData()
         {
             InitializeComponent();
+            progBarImport.Visible = false;
         }
 
+        /// <summary>
+        /// Handles the Click event of the butExcel control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
         private void butExcel_Click(object sender, EventArgs e)
         {
+            txtExcel.Clear();
             openFileDialog_Excel.Title = "Please choose excel data source file";
             openFileDialog_Excel.FileName = "";
-            openFileDialog_Excel.Filter = "*.xls|*.xls";  
-            if(openFileDialog_Excel.ShowDialog() == DialogResult.OK)
-            {
-                
-            }
+            openFileDialog_Excel.Filter = "*.xls|*.xls";
+            if (openFileDialog_Excel.ShowDialog() == DialogResult.OK)
+                txtExcel.Text = openFileDialog_Excel.FileName;
         }
 
+        /// <summary>
+        /// Handles the Click event of the butS3GTDB control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
         private void butS3GTDB_Click(object sender, EventArgs e)
         {
+            txtS3GTDB.Clear();
             openFileDialog_S3GTDB.Title = "Please choose database of S3GT";
             openFileDialog_S3GTDB.FileName = "";
             openFileDialog_S3GTDB.Filter = "s3gt_db*.mdb|*.mdb";
-            openFileDialog_S3GTDB.InitialDirectory = "C:\\Program Files\\S3_JCD\\S3GT\\datasource\\"; // default path
-            if(openFileDialog_S3GTDB.ShowDialog()== DialogResult.OK)
-            {
-                
-            }
-
+            //openFileDialog_S3GTDB.InitialDirectory = "C:\\Program Files\\S3_JCD\\S3GT\\datasource\\"; // default path
+            openFileDialog_S3GTDB.InitialDirectory = System.IO.Directory.GetCurrentDirectory().ToString() + "\\datasource\\";
+            if (openFileDialog_S3GTDB.ShowDialog() == DialogResult.OK)
+                txtS3GTDB.Text = openFileDialog_S3GTDB.FileName;
         }
 
+        /// <summary>
+        /// Progresses the bar handle.
+        /// </summary>
+        private void progressBarHandle()
+        {
+            //start PrgressBar
+            progBarImport.Visible = true;
+            int i = 0;
+            for (; i < progBarImport.Maximum; i++)
+                progBarImport.Increment(progBarImport.Step);
+        }
+        /// <summary>
+        /// Handles the Click event of the butConvert control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
         private void butConvert_Click(object sender, EventArgs e)
         {
+            if (txtExcel.Text == "")
+            {
+                MessageBox.Show("Excel file is invalid!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (txtS3GTDB.Text == "")
+            {
+                MessageBox.Show("S3GT_DB file is invalid!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //Read data from Excel file
+            progressBarHandle();
+            BUS_Grammar busGram = new BUS_Grammar(Constants.DATABASE_PATH);
+            DTO_Grammar[] gramCards = busGram.ReadExcelFile(txtExcel.Text);
+            if (gramCards == null)
+            {
+                if (MessageBox.Show("Invalid Excel data!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    progBarImport.Value = 0;
+                    progBarImport.Visible = false;
+                }
+            }
+            else
+            {
+                //Import to S3GT_DB    
+                //Get the kyu
+                int iKyu = 0;
+                if (rad1kyu.Checked)
+                    iKyu = 1;
+                else if (rad2kyu.Checked)
+                    iKyu = 2;
+                else if (rad3kyu.Checked)
+                    iKyu = 3;
+                else if (rad4kyu.Checked)
+                    iKyu = 4;
+                //Get the methods of add
+                if (radDelete.Checked)
+                    busGram.DeleteGrammarCards(iKyu, txtS3GTDB.Text);
+                if (!busGram.InsertGrammarCards(gramCards, iKyu.ToString(), txtS3GTDB.Text))
+                {
+                    if (MessageBox.Show("Import data fail!!!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                    {
+                        progBarImport.Value = 0;
+                        progBarImport.Visible = false;
+                    }
+                }
+                else
+                {
+                    if (MessageBox.Show("Import data sucessfull!!!", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                    {
+                        progBarImport.Value = 0;
+                        progBarImport.Visible = false;
+                    }
+                }
+
+            }
 
         }
     }
