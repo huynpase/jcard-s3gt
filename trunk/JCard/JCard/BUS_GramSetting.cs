b640f6dd738739804c23274fbe06efbee3e9cc9a
vietnguyen0717 <vietnguyen0717@1fbad98d-3cfb-ac3b-e82d-718aab666f2b>
2011-03-04 03:38:33 +0000

    UPdate BUS_GramSetting.cs file

--- trunk/JCard/JCard/BUS_GramSetting.cs
+++ trunk/JCard/JCard/BUS_GramSetting.cs
@@ -6,5 +6,18 @@ namespace JCard
 {
     class BUS_GramSetting
     {
+        public DTO_GramSetting ReadGramSetting(string filePath)
+        {
+            DTO_GramSetting dtoGramSett;
+            DAO_GramSetting daoGramSett = new DAO_GramSetting();
+            dtoGramSett = daoGramSett.ReadGramSetting(filePath);
+            return dtoGramSett;
+        }
+
+        public void SaveGramSetting(DTO_GramSetting dtoGramSett, string filePath)
+        {
+            DAO_GramSetting daoGramSett = new DAO_GramSetting();
+            daoGramSett.WriteGramSettings(dtoGramSett, filePath);
+        }
     }
 }
