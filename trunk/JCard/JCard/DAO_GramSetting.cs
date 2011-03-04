f16d0ffeaf0d38b4ef2a40d591af4521d72cd467
vietnguyen0717 <vietnguyen0717@1fbad98d-3cfb-ac3b-e82d-718aab666f2b>
2011-03-04 18:59:59 +0000

    - Update file DAO_GramSetting.cs - Cap nhat tinh nang Save va load file ini cho form fGrammarSetting

--- trunk/JCard/JCard/DAO_GramSetting.cs
+++ trunk/JCard/JCard/DAO_GramSetting.cs
@@ -15,57 +15,50 @@ namespace JCard
         {
             DTO_GramSetting gramSettDTO = new DTO_GramSetting();
             IniFile ini = new IniFile(iniPath);
-
-            // 1. Các hàm bên dưới chưa nhận kết quả.
-            // ví dụ: gramSettDTO.Top = int.Parse(ini.ReadValue("Position", "Top", Constants.Top));
-
-            // 2. Bỏ hết dấu "[" và "]"
-
-            ini.ReadValue("[Position]", "Top", Constants.Top);
-            ini.ReadValue("[Position]", "Left",Constants.Left);
-            ini.ReadValue("[Sample]","BackColor",Constants.BackColor);
-            ini.ReadValue("[Sample]", "ForeColor", Constants.ForeColor);
-            ini.ReadValue("[Sample]", "Width", Constants.Width);
-            ini.ReadValue("[JPMeaning]", "JP_BackColor", Constants.JP_BackColor);
-            ini.ReadValue("[JPMeaning]", "JP_ForeColor", Constants.JP_ForeColor);
-            ini.ReadValue("[JPMeaning]", "JP_Width", Constants.JP_Width);
-            ini.ReadValue("[JPMeaning]", "JP_IsDisplayed", Constants.JP_IsDisplayed);
-            ini.ReadValue("[VNMeaning]", "VN_BackColor", Constants.VN_BackColor);
-            ini.ReadValue("[VNMeaning]", "VN_ForeColor", Constants.VN_ForeColor);
-            ini.ReadValue("[VNMeaning]", "VN_Width", Constants.VN_Width);
-            ini.ReadValue("[VNMeaning]", "VN_IsDisplayed", Constants.VN_IsDisplayed);
-            ini.ReadValue("[Example]", "Ex_BackColor", Constants.Ex_BackColor);
-            ini.ReadValue("[Example]", "Ex_ForeColor", Constants.Ex_ForeColor);
-            ini.ReadValue("[Example]", "Ex_NoOfDisplay", Constants.Ex_NoOfDisplay);
-            ini.ReadValue("[Example]", "Ex_DisplayTime", Constants.Ex_DisplayTime);
-            ini.ReadValue("[Example]", "Ex_DelayTime", Constants.Ex_DelayTime);
+            gramSettDTO.Top = int.Parse(ini.ReadValue("Position", "Top", Constants.Top));
+            gramSettDTO.Left = int.Parse(ini.ReadValue("Position", "Left",Constants.Left));
+            gramSettDTO.BackColor = int.Parse(ini.ReadValue("Sample","BackColor",Constants.BackColor));
+            gramSettDTO.ForeColor = int.Parse(ini.ReadValue("Sample", "ForeColor", Constants.ForeColor));
+            gramSettDTO.Width = int.Parse(ini.ReadValue("Sample", "Width", Constants.Width));
+            gramSettDTO.JP_BackColor = int.Parse(ini.ReadValue("JPMeaning", "JP_BackColor", Constants.JP_BackColor));
+            gramSettDTO.JP_ForeColor = int.Parse(ini.ReadValue("JPMeaning", "JP_ForeColor", Constants.JP_ForeColor));
+            gramSettDTO.JP_Width = int.Parse(ini.ReadValue("JPMeaning", "JP_Width", Constants.JP_Width));
+            gramSettDTO.JP_Isdisplayed = bool.Parse(ini.ReadValue("JPMeaning", "JP_IsDisplayed", Constants.JP_IsDisplayed));
+            gramSettDTO.VN_BackColor = int.Parse(ini.ReadValue("VNMeaning", "VN_BackColor", Constants.VN_BackColor));
+            gramSettDTO.VN_ForeColor = int.Parse(ini.ReadValue("VNMeaning", "VN_ForeColor", Constants.VN_ForeColor));
+            gramSettDTO.VN_Width = int.Parse(ini.ReadValue("VNMeaning", "VN_Width", Constants.VN_Width));
+            gramSettDTO.VN_IsDisplayed = bool.Parse(ini.ReadValue("VNMeaning", "VN_IsDisplayed", Constants.VN_IsDisplayed));
+            gramSettDTO.Ex_BackColor = int.Parse(ini.ReadValue("Example", "Ex_BackColor", Constants.Ex_BackColor));
+            gramSettDTO.Ex_ForeColor = int.Parse(ini.ReadValue("Example", "Ex_ForeColor", Constants.Ex_ForeColor));
+            gramSettDTO.Ex_NoOfDisplay = int.Parse(ini.ReadValue("Example", "Ex_NoOfDisplay", Constants.Ex_NoOfDisplay));
+            gramSettDTO.Ex_DisplayTime = int.Parse(ini.ReadValue("Example", "Ex_DisplayTime", Constants.Ex_DisplayTime));
+            gramSettDTO.Ex_DelayTime = int.Parse(ini.ReadValue("Example", "Ex_DelayTime", Constants.Ex_DelayTime));
 
             return gramSettDTO;
         }
 
         public void WriteGramSettings(DTO_GramSetting gramSettDTO, string iniPath)
         {
-            // 3. Bỏ hết dấu "[" và "]"
 
             IniFile ini = new IniFile(iniPath);
-            ini.WriteValue("[Position]","Top",gramSettDTO.Top.ToString());
-            ini.WriteValue("[Position]", "Left", gramSettDTO.Left.ToString());
-            ini.WriteValue("[Sample]", "BackColor", gramSettDTO.BackColor.ToString());
-            ini.WriteValue("[Sample]", "ForeColor", gramSettDTO.ForeColor.ToString());
-            ini.WriteValue("[Sample]", "Width", gramSettDTO.Width.ToString());
-            ini.WriteValue("[JPMeaning]", "JP_BackColor", gramSettDTO.JP_BackColor.ToString());
-            ini.WriteValue("[JPMeaning]", "JP_ForeColor", gramSettDTO.JP_ForeColor.ToString());
-            ini.WriteValue("[JPMeaning]", "JP_Width", gramSettDTO.JP_Width.ToString());
-            ini.WriteValue("[JPMeaning]", "JP_IsDisplayed", gramSettDTO.JP_Isdisplayed.ToString());
-            ini.WriteValue("[VNMeaning]", "VN_BackColor", gramSettDTO.VN_BackColor.ToString());
-            ini.WriteValue("[VNMeaning]", "VN_ForeColor", gramSettDTO.VN_ForeColor.ToString());
-            ini.WriteValue("[VNMeaning]", "VN_Width", gramSettDTO.VN_Width.ToString());
-            ini.WriteValue("[VNMeaning]", "VN_IsDisplayed", gramSettDTO.VN_IsDisplayed.ToString());
-            ini.WriteValue("[Example]", "Ex_BackColor", gramSettDTO.Ex_BackColor.ToString());
-            ini.WriteValue("[Example]", "Ex_ForeColor", gramSettDTO.Ex_ForeColor.ToString());
-            ini.WriteValue("[Example]", "Ex_NoOfDisplay", gramSettDTO.Ex_NoOfDisplay.ToString());
-            ini.WriteValue("[Example]", "Ex_DisplayTime", gramSettDTO.Ex_DisplayTime.ToString());
-            ini.WriteValue("[Example]", "Ex_DelayTime", gramSettDTO.Ex_DelayTime.ToString());
+            ini.WriteValue("Position","Top",gramSettDTO.Top.ToString());
+            ini.WriteValue("Position", "Left", gramSettDTO.Left.ToString());
+            ini.WriteValue("Sample", "BackColor", gramSettDTO.BackColor.ToString());
+            ini.WriteValue("Sample", "ForeColor", gramSettDTO.ForeColor.ToString());
+            ini.WriteValue("Sample", "Width", gramSettDTO.Width.ToString());
+            ini.WriteValue("JPMeaning", "JP_BackColor", gramSettDTO.JP_BackColor.ToString());
+            ini.WriteValue("JPMeaning", "JP_ForeColor", gramSettDTO.JP_ForeColor.ToString());
+            ini.WriteValue("JPMeaning", "JP_Width", gramSettDTO.JP_Width.ToString());
+            ini.WriteValue("JPMeaning", "JP_IsDisplayed", gramSettDTO.JP_Isdisplayed.ToString());
+            ini.WriteValue("VNMeaning", "VN_BackColor", gramSettDTO.VN_BackColor.ToString());
+            ini.WriteValue("VNMeaning", "VN_ForeColor", gramSettDTO.VN_ForeColor.ToString());
+            ini.WriteValue("VNMeaning", "VN_Width", gramSettDTO.VN_Width.ToString());
+            ini.WriteValue("VNMeaning", "VN_IsDisplayed", gramSettDTO.VN_IsDisplayed.ToString());
+            ini.WriteValue("Example", "Ex_BackColor", gramSettDTO.Ex_BackColor.ToString());
+            ini.WriteValue("Example", "Ex_ForeColor", gramSettDTO.Ex_ForeColor.ToString());
+            ini.WriteValue("Example", "Ex_NoOfDisplay", gramSettDTO.Ex_NoOfDisplay.ToString());
+            ini.WriteValue("Example", "Ex_DisplayTime", gramSettDTO.Ex_DisplayTime.ToString());
+            ini.WriteValue("Example", "Ex_DelayTime", gramSettDTO.Ex_DelayTime.ToString());
         }
     }
 }
