7dcaff7ce396d05290c5ad3e12fe580eabb7a612
lamchiphuong <lamchiphuong@gmail.com>
2011-03-13 13:47:38 +0000

    Update and fix bug following comment of MrTrieu

--- JCard/JCard/About.Designer.cs
+++ JCard/JCard/About.Designer.cs
@@ -1,4 +1,4 @@
-namespace JCard
+﻿namespace JCard
 {
     partial class About
     {
@@ -28,23 +28,26 @@ namespace JCard
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 40);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(217, 13);
+            this.label1.Size = new System.Drawing.Size(218, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Copyright 2010 by PhuongHD@fsoft.com.vn";
+            this.label1.Text = "Copyright 2011 by JCD - FSOFT HCM\r\n";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(95, 116);
+            this.button1.Location = new System.Drawing.Point(97, 168);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 23);
             this.button1.TabIndex = 1;
@@ -55,20 +58,35 @@ namespace JCard
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(92, 73);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(94, 38);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.Size = new System.Drawing.Size(104, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Mobile: 01687592977";
+            this.label2.Text = "Project members:\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(80, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 90);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Trần Đức Hải Triều\r\nHuỳnh Đức Phương\r\nHuỳnh Nhật Minh\r\nNguyễn Quôc Việt\r\nNguyễn X" +
+                "uân Quyền\r\nLê Anh Dũng\r\n";
             // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 174);
+            this.ClientSize = new System.Drawing.Size(292, 203);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "About";
@@ -84,5 +102,6 @@ namespace JCard
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
