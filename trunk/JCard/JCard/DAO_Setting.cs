ce582ad743ed9d1c184e04b05f4a07100575e173
vietnguyen0717 <vietnguyen0717@1fbad98d-3cfb-ac3b-e82d-718aab666f2b>
2011-04-04 15:10:07 +0000

    Update Vocabulary Setting

--- trunk/JCard/JCard/DAO_Setting.cs
+++ trunk/JCard/JCard/DAO_Setting.cs
@@ -12,109 +12,66 @@ namespace JCard
 {
     class DAO_Setting
     {
-        
-        //public DTO_Setting ReadSetting(string filePath)
-        //{
-        //    DTO_Setting sttDTO = new DTO_Setting();
-        //    ArrayList arrSetting = new ArrayList();            
-        //    string temp = "";
-        //    string subTemp ="";
-        //    try
-        //    {
-        //        string FilePath = filePath;
-        //        FileStream fStreamRead = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
-        //        StreamReader fStreamReader = new StreamReader(fStreamRead, Encoding.Default);
-        //        //Doc cho den het file neu thuoc loai nao thi gan gia tri tuong an vao bien setting
-        //        while (fStreamReader.Peek() >= 0)
-        //        {
-        //            temp = fStreamReader.ReadLine();                  
-        //            if(temp.Contains("PositionVOC="))
-        //            {
-        //                subTemp = "PositionVOC=";
-        //                sttDTO.PositionVOC = temp.Remove(0, subTemp.Length);
-        //            }
-        //            else if (temp.Contains("DisplayTimeVOC="))
-        //            {
-        //                subTemp = "DisplayTimeVOC=";
-        //                sttDTO.DisplayTimeVOC = int.Parse(temp.Remove(0, subTemp.Length));
-        //            }
-        //            else if (temp.Contains("WaitingTimeVOC="))
-        //            {
-        //                subTemp = "WaitingTimeVOC=";
-        //                sttDTO.WaitingTimeVOC = int.Parse(temp.Remove(0, subTemp.Length));
-        //            }
-        //            else if (temp.Contains("FontSizeKanjVOC="))
-        //            {
-        //                subTemp = "FontSizeKanjVOC=";
-        //                sttDTO.FontSizeKanjiVOC= int.Parse(temp.Remove(0, subTemp.Length));
-        //            }
-        //            else if (temp.Contains("FontSizeHiraganaVOC="))
-        //            {
-        //                subTemp = "FontSizeHiraganaVOC=";
-        //                sttDTO.FontSizeKanaVOC = int.Parse(temp.Remove(0, subTemp.Length));
-        //            }
-        //            else if (temp.Contains("FontSizeImiVOC="))
-        //            {
-        //                subTemp = "FontSizeImiVOC=";
-        //                sttDTO.FontSizeImiVOC= int.Parse(temp.Remove(0, subTemp.Length));
-        //            }
-
-        //        }                
-        //        fStreamRead.Close();
-        //        fStreamReader.Close();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw new Exception("Can't read the file");
-        //    }
-        //    return sttDTO;
-        //}
-
-        //Ghi setting ra file neu nguoi dung thay doi
-
        public DTO_Setting ReadSetting(string filePath)
        {
            DTO_Setting dto = new DTO_Setting();
            IniFile ini = new IniFile(filePath);
-
            try
            {
-               dto.PositionVOC = ini.ReadValue("Jcard", "PositionVOC", "BR");
-               dto.DisplayTimeVOC = float.Parse(ini.ReadValue("Jcard", "DisplayTimeVOC", "5"));
-               dto.WaitingTimeVOC = float.Parse(ini.ReadValue("Jcard", "WaitingTimeVOC", "3"));
-               dto.FontSizeKanjiVOC = float.Parse(ini.ReadValue("Jcard", "FontSizeKanjVOC", "19"));
-               dto.FontSizeKanaVOC = float.Parse(ini.ReadValue("Jcard", "FontSizeHiraganaVOC", "16"));
-               dto.FontSizeImiVOC = float.Parse(ini.ReadValue("Jcard", "FontSizeImiVOC", "13"));
-
-               dto.FontSizeHanNom = float.Parse(ini.ReadValue("Jcard", "FontSizeHanNom", "16"));
+               dto.PositionVOC = ini.ReadValue("Vocabulary", "PositionVOC", Constants.PositionVOC);
+               dto.DisplayTimeVOC = int.Parse(ini.ReadValue("Vocabulary", "DisplayTimeVOC", Constants.DisplayTimeVOC));
+               dto.WaitingTimeVOC = int.Parse(ini.ReadValue("Vocabulary", "WaitingTimeVOC", Constants.WaitingTimeVOC));
+               dto.Kanji_BackColor = int.Parse(ini.ReadValue("Kanji", "Kanji_BackColor", Constants.Kanji_BackColor));
+               dto.Kanji_FontColor = int.Parse(ini.ReadValue("Kanji", "Kanji_FontColor", Constants.Kanji_FontColor));
+               dto.Kanji_Fontsize = int.Parse(ini.ReadValue("Kanji", "Kanji_Fontsize", Constants.Kanji_Fontsize));
+               dto.Kanji_IsDisplayed = bool.Parse(ini.ReadValue("Kanji", "Kanji_IsDisplayed", Constants.Kanji_IsDisplayed));
+               dto.Hiragana_BackColor = int.Parse(ini.ReadValue("Hiragana", "Hiragana_BackColor", Constants.Hiragana_BackColor));
+               dto.Hiragana_FontColor = int.Parse(ini.ReadValue("Hiragana", "Hiragana_FontColor", Constants.Hiragana_FontColor));
+               dto.Hiragana_Fontsize = int.Parse(ini.ReadValue("Hiragana", "Hiragana_Fontsize", Constants.Hiragana_Fontsize));
+               dto.Hiragana_IsDisplayed = bool.Parse(ini.ReadValue("Hiragana", "Hiragana_IsDisplayed", Constants.Hiragana_IsDisplayed));
+               dto.Meaning_BackColor = int.Parse(ini.ReadValue("Meaning", "Meaning_BackColor", Constants.Meaning_BackColor));
+               dto.Meaning_FontColor = int.Parse(ini.ReadValue("Meaning", "Meaning_FontColor", Constants.Meaning_FontColor));
+               dto.Meaning_Fontsize = int.Parse(ini.ReadValue("Meaning", "Meaning_Fontsize", Constants.Meaning_Fontsize));
+               dto.Meaning_IsDisplayed = bool.Parse(ini.ReadValue("Meaning", "Meaning_IsDisplayed",Constants.Meaning_IsDisplayed));
            }
            catch 
            {
-               
                dto.PositionVOC = Constants.PositionVOC;
-               dto.DisplayTimeVOC = Constants.DisplayTimeVOC;
-               dto.WaitingTimeVOC = Constants.WaitingTimeVOC ;
-               dto.FontSizeKanjiVOC = Constants.FontSizeKanjiVOC;
-               dto.FontSizeImiVOC = Constants.FontSizeImiVOC;
-               dto.FontSizeKanaVOC = Constants.FontSizeKanaVOC;
-               dto.FontSizeHanNom = Constants.FontSizeHanNom;
-
-               SaveSetting(dto,filePath);
-              
+               dto.DisplayTimeVOC = int.Parse(Constants.DisplayTimeVOC);
+               dto.WaitingTimeVOC = int.Parse(Constants.WaitingTimeVOC);
+               dto.Kanji_BackColor = int.Parse(Constants.Kanji_BackColor);
+               dto.Kanji_FontColor = int.Parse(Constants.Kanji_FontColor);
+               dto.Kanji_Fontsize = int.Parse(Constants.Kanji_Fontsize);
+               dto.Kanji_IsDisplayed = bool.Parse(Constants.Kanji_IsDisplayed);
+               dto.Hiragana_BackColor = int.Parse(Constants.Hiragana_BackColor);
+               dto.Hiragana_FontColor = int.Parse(Constants.Hiragana_FontColor);
+               dto.Hiragana_Fontsize = int.Parse(Constants.Hiragana_Fontsize);
+               dto.Hiragana_IsDisplayed = bool.Parse(Constants.Hiragana_IsDisplayed);
+               dto.Meaning_BackColor = int.Parse(Constants.Meaning_BackColor);
+               dto.Meaning_FontColor = int.Parse(Constants.Meaning_FontColor);
+               dto.Meaning_Fontsize = int.Parse(Constants.Meaning_Fontsize);
+               dto.Meaning_IsDisplayed = bool.Parse(Constants.Meaning_IsDisplayed);
            }
-         
            return dto;
        }
         public void SaveSetting(DTO_Setting settingDTO, string filePath)
         {
             IniFile ini = new IniFile(filePath);
-            ini.WriteValue("Jcard", "PositionVOC", settingDTO.PositionVOC);
-            ini.WriteValue("Jcard", "DisplayTimeVOC", ((int)settingDTO.DisplayTimeVOC).ToString());
-            ini.WriteValue("Jcard", "WaitingTimeVOC", ((int)settingDTO.WaitingTimeVOC).ToString());
-            ini.WriteValue("Jcard", "FontSizeKanjVOC", ((int)settingDTO.FontSizeKanjiVOC).ToString());
-            ini.WriteValue("Jcard", "FontSizeHiraganaVOC", ((int)settingDTO.FontSizeKanaVOC).ToString());
-            ini.WriteValue("Jcard", "FontSizeImiVOC", ((int)settingDTO.FontSizeImiVOC).ToString());
-            ini.WriteValue("Jcard", "FontSizeHanNom", ((int)settingDTO.FontSizeHanNom).ToString());
+            ini.WriteValue("Vocabulary", "PositionVOC", settingDTO.PositionVOC);
+            ini.WriteValue("Vocabulary", "DisplayTimeVOC", settingDTO.DisplayTimeVOC.ToString());
+            ini.WriteValue("Vocabulary", "WaitingTimeVOC", settingDTO.WaitingTimeVOC.ToString());
+            ini.WriteValue("Kanji", "Kanji_BackColor", settingDTO.Kanji_BackColor.ToString());
+            ini.WriteValue("Kanji", "Kanji_FontColor", settingDTO.Kanji_FontColor.ToString());
+            ini.WriteValue("Kanji", "Kanji_Fontsize", settingDTO.Kanji_Fontsize.ToString());
+            ini.WriteValue("Kanji", "Kanji_IsDisplayed", settingDTO.Kanji_IsDisplayed.ToString());
+            ini.WriteValue("Hiragana", "Hiragana_BackColor", settingDTO.Hiragana_BackColor.ToString());
+            ini.WriteValue("Hiragana", "Hiragana_FontColor", settingDTO.Hiragana_FontColor.ToString());
+            ini.WriteValue("Hiragana", "Hiragana_Fontsize", settingDTO.Hiragana_Fontsize.ToString());
+            ini.WriteValue("Hiragana", "Hiragana_IsDisplayed", settingDTO.Hiragana_IsDisplayed.ToString());
+            ini.WriteValue("Meaning", "Meaning_BackColor", settingDTO.Meaning_BackColor.ToString());
+            ini.WriteValue("Meaning", "Meaning_FontColor", settingDTO.Meaning_FontColor.ToString());
+            ini.WriteValue("Meaning", "Meaning_Fontsize", settingDTO.Meaning_Fontsize.ToString());
+            ini.WriteValue("Meaning", "Meaning_IsDisplayed", settingDTO.Meaning_IsDisplayed.ToString());
         }
     }
 }
